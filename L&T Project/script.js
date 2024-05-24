cd0fc313f4c14fbed145b0b5918ebc1241f03f15
Aashish <rebelraaz2@gmail.com>
2024-05-24 20:31:05 +0530

    final commit

---
+++ L&T Project/script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var items = document.querySelectorAll('.banner-item');
+    var currentImageIndex = 0;
+
+    function showImage(index) {
+        items.forEach(function (item) {
+            item.classList.remove('active');
+        });
+        items[index].classList.add('active');
+    }
+
+    function nextImage() {
+        currentImageIndex = (currentImageIndex + 1) % items.length;
+        showImage(currentImageIndex);
+    }
+
+    // Automatically switch images every 3 seconds
+    setInterval(nextImage, 2000);
+});
+
